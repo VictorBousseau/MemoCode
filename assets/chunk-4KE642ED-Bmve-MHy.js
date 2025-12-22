4a651521659589af2e83755587e64f4e109838aa
Bousseau Victor <bousseauvictor49@gmail.com>
2025-12-22 20:14:40 +0100

    Updates

---
+++ assets/chunk-4KE642ED-Bmve-MHy.js
@@ -0,0 +1,1 @@
+import{m as e}from"./index-CHW6z1L2.js";function a(c,t){c.accDescr&&t.setAccDescription?.(c.accDescr),c.accTitle&&t.setAccTitle?.(c.accTitle),c.title&&t.setDiagramTitle?.(c.title)}e(a,"populateCommonDb");export{a as c};
---
+++ assets/chunk-VSLJSFIP-BopfZbBm.js
@@ -0,0 +1,1 @@
+import{m as s}from"./index-CHW6z1L2.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as s};
