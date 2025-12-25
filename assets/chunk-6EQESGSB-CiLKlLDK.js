3967ea72e864d94677d999668c09bbcce98da175
Bousseau Victor <bousseauvictor49@gmail.com>
2025-12-25 20:03:41 +0100

    Updates

--- assets/chunk-6EQESGSB-CiLKlLDK.js
+++
@@ -1,1 +0,0 @@
-import{m,i as e}from"./index-DBP3O4Pp.js";var r=m((o,t)=>{let a;return t==="sandbox"&&(a=e("#i"+o)),(t==="sandbox"?e(a.nodes()[0].contentDocument.body):e("body")).select(`[id="${o}"]`)},"getDiagramElement");export{r as m};
