15dc8bcc7d06f4f86e5c3d63572de1f55dbbc5db
Bousseau Victor <bousseauvictor49@gmail.com>
2026-03-07 13:14:10 +0100

    Updates

---
+++ assets/chunk-VSLJSFIP-Bw1G2Lq9.js
@@ -0,0 +1,1 @@
+import{m as s}from"./index-Dc41Gbuh.js";var t,e=(t=class{constructor(i){this.init=i,this.records=this.init()}reset(){this.records=this.init()}},s(t,"ImperativeState"),t);export{e as s};
